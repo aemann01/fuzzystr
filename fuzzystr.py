70c8e1d8d1e45d5e7b65bcd6df7903003d737cad
aemann01 <aemann01@ou.edu>
2020-03-16 15:27:48 -0500

    added comments

--- fuzzystr.py
+++ fuzzystr.py
@@ -20,6 +20,7 @@ parser.add_argument('-e', '--edit', help='desired edit distance cutoff (float)',
 args = parser.parse_args()
 
 def fuzzystr():
+	# initialize empty dataframe
 	df = pd.DataFrame(columns=["seqid", "seq", "trim", "motif", "length", "trimlen", "locus", "fwstart", "fwend", "rvstart", "rvend", "fwdist", "rvdist", "fwmatch_seq", "rvmatch_seq"])
 	record_count = 0
 	for record in SeqIO.parse(args.fasta, "fasta"):
@@ -28,9 +29,11 @@ def fuzzystr():
 		length = len(record.seq)
 		record_count += 1
 
+		# print record count to screen every 100 loops
 		if record_count % 100 == 0:
 			print(record_count)
 
+		# iterate over primer file for each record
 		with open(args.primers, "r") as f:
 			for line in f:
 				fields = line.strip().split()
@@ -43,8 +46,10 @@ def fuzzystr():
 				fwmatch = find_near_matches(fwprimer, seq, max_l_dist=ed1)
 				rvmatch = find_near_matches(rvprimer, seq, max_l_dist=ed2)
 
+				# break if either forward or reverse match are not found
 				if len(fwmatch) == 0 or len(rvmatch) == 0:
 					break
+				# for both forward and reverse, pick index of lowest edit distance match
 				else:
 					fwindex = []
 					for i in range(0, len(fwmatch)):
@@ -55,7 +60,6 @@ def fuzzystr():
 					fwseq = fwmatch[fwindex.index(min(fwindex))].matched
 	
 					rvindex = []
-					print(rvmatch)
 					for i in range(0, len(rvmatch)):
 						rvindex.append(rvmatch[i].dist)
 					rvstart = rvmatch[rvindex.index(min(rvindex))].start
@@ -63,8 +67,10 @@ def fuzzystr():
 					rvdist = rvmatch[rvindex.index(min(rvindex))].dist
 					rvseq = rvmatch[rvindex.index(min(rvindex))].matched
 
+				# break if reverse is downstream to forward primer
 				if rvend < fwstart:
 					break
+				# trim region
 				else:
 					rvend += 1
 					trim = seq[fwstart:rvend]
@@ -75,9 +81,9 @@ def fuzzystr():
 				motif_check = find_near_matches(motif, trim, max_l_dist=ed3)
 				if motif_check == 0:
 					break
-
-				df = df.append({"seqid": seqid, "seq": seq, "trim": trim, "trimlen": trimlen, "motif": motif, "length": length, "locus": locus, "fwstart": fwstart, "fwend": fwend, "rvstart": rvstart, "rvend": rvend, "fwdist": fwdist, "rvdist": rvdist, "fwmatch_seq": fwseq, "rvmatch_seq": rvseq}, ignore_index=True)
-
+			# populate dataframe
+			df = df.append({"seqid": seqid, "seq": seq, "trim": trim, "trimlen": trimlen, "motif": motif, "length": length, "locus": locus, "fwstart": fwstart, "fwend": fwend, "rvstart": rvstart, "rvend": rvend, "fwdist": fwdist, "rvdist": rvdist, "fwmatch_seq": fwseq, "rvmatch_seq": rvseq}, ignore_index=True)
+	# write to file
 	with open(args.output, "w") as output:
 		df.to_csv(output, index=False)
 
