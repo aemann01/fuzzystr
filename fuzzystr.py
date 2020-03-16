1c6d4992cecd741bcbd1d883f9ced5dcb12aae41
aemann01 <aemann01@ou.edu>
2020-03-16 15:09:52 -0500

    modified to work with fasta

--- fuzzystr.py
+++ fuzzystr.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 '''
-Fuzzy match primer protocol for STR project. Use: python3 test_script.py -f input.fastq -p primerFile.txt -o output.txt
+Fuzzy match primer protocol for STR project. Use: python3 fuzzystr.py -f input.fasta -p primerFile.txt -o output.txt
 '''
 
 import pandas as pd
@@ -13,7 +13,7 @@ parser = argparse.ArgumentParser()
 from fuzzysearch import find_near_matches
 from Bio import SeqIO
 
-parser.add_argument('-f', '--fastq', help='input fastq file', required=True)
+parser.add_argument('-f', '--fasta', help='input fasta file', required=True)
 parser.add_argument('-p', '--primers', help='list of primers', required=True)
 parser.add_argument('-o', '--output', help='output file', default='output.txt')
 parser.add_argument('-e', '--edit', help='desired edit distance cutoff (float)', default='0.25')
@@ -22,7 +22,7 @@ args = parser.parse_args()
 def fuzzystr():
 	df = pd.DataFrame(columns=["seqid", "seq", "trim", "motif", "length", "trimlen", "locus", "fwstart", "fwend", "rvstart", "rvend", "fwdist", "rvdist", "fwmatch_seq", "rvmatch_seq"])
 	record_count = 0
-	for record in SeqIO.parse(args.fastq, "fastq"):
+	for record in SeqIO.parse(args.fasta, "fasta"):
 		seqid = record.name 
 		seq = record.seq
 		length = len(record.seq)
@@ -43,63 +43,54 @@ def fuzzystr():
 				fwmatch = find_near_matches(fwprimer, seq, max_l_dist=ed1)
 				rvmatch = find_near_matches(rvprimer, seq, max_l_dist=ed2)
 
-				if fwmatch == []:
+				if len(fwmatch) == 0 or len(rvmatch) == 0:
 					break
 				else:
-					distances = []
+					fwindex = []
 					for i in range(0, len(fwmatch)):
-						distances.append(fwmatch[i].dist)
-					fwstart = fwmatch[distances.index(min(distances))].start
-					fwend = fwmatch[distances.index(min(distances))].end
-					fwdist = fwmatch[distances.index(min(distances))].dist
-					fwseq = fwmatch[distances.index(min(distances))].matched
+						fwindex.append(fwmatch[i].dist)
+					fwstart = fwmatch[fwindex.index(min(fwindex))].start
+					fwend = fwmatch[fwindex.index(min(fwindex))].end
+					fwdist = fwmatch[fwindex.index(min(fwindex))].dist
+					fwseq = fwmatch[fwindex.index(min(fwindex))].matched
+	
+					rvindex = []
+					print(rvmatch)
+					for i in range(0, len(rvmatch)):
+						rvindex.append(rvmatch[i].dist)
+					rvstart = rvmatch[rvindex.index(min(rvindex))].start
+					rvend = rvmatch[rvindex.index(min(rvindex))].end
+					rvdist = rvmatch[rvindex.index(min(rvindex))].dist
+					rvseq = rvmatch[rvindex.index(min(rvindex))].matched
 
-				if rvmatch == []:
-					rvhit = "NA"
-					rvstart = "NA"
-					rvend = "NA"
-					rvdist = "NA"
-					rvseq = "NA"
+				if rvend < fwstart:
+					break
 				else:
-					distances = []
-					for i in range(0, len(rvmatch)):	
-						distances.append(rvmatch[i].dist)
-					rvstart = rvmatch[distances.index(min(distances))].start
-					rvend = rvmatch[distances.index(min(distances))].end
-					rvdist = rvmatch[distances.index(min(distances))].dist
-					rvseq = rvmatch[distances.index(min(distances))].matched
-
-				if rvstart == "NA":
-					trim = seq[fwstart:]
-				elif fwstart < rvend:
 					rvend += 1
 					trim = seq[fwstart:rvend]
-				else:
-					fwend += 1
-					trim = seq[rvstart:fwend]
 				trimlen = len(trim)
+					
+				#check if motif is found at least once in the sequence
+				ed3 = round(len(motif)*float(args.edit))
+				motif_check = find_near_matches(motif, trim, max_l_dist=ed3)
+				if motif_check == 0:
+					break
 
-			df = df.append({"seqid": seqid, "seq": seq, "trim": trim, "motif": motif, "length": length, "trimlen": trimlen, "locus": locus, "fwstart": fwstart, "fwend": fwend, "rvstart": rvstart, "rvend": rvend, "fwdist": fwdist, "rvdist": rvdist, "fwmatch_seq": fwseq, "rvmatch_seq": rvseq}, ignore_index=True)
+				df = df.append({"seqid": seqid, "seq": seq, "trim": trim, "trimlen": trimlen, "motif": motif, "length": length, "locus": locus, "fwstart": fwstart, "fwend": fwend, "rvstart": rvstart, "rvend": rvend, "fwdist": fwdist, "rvdist": rvdist, "fwmatch_seq": fwseq, "rvmatch_seq": rvseq}, ignore_index=True)
 
 	with open(args.output, "w") as output:
 		df.to_csv(output, index=False)
 
 fuzzystr()
 
-#def motifcount():
-	#res = [seq[i:j] for i in range(len(seq)) for j in range(i+1, len(seq) + 1)]
-	#countdict = {i:res.count(i) for i in res
 
 
 
 
-#def alignref():
 
-#def filter():
-## TO DO: length and motif filtering function
 
-#def strstats():
-## TO DO: get the rep # and motif information, generate statistics
+
+
 
 
 
