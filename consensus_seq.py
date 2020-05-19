0c28531ce74bc9481cb42bd4d2a41c7523abdbe3
aemann01 <aemann01@ou.edu>
2020-05-19 16:26:30 -0500

    clean up

--- consensus_seq.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-
-import sys
-from Bio import AlignIO
-from Bio.Align import AlignInfo
-
-alignment = AlignIO.read(sys.argv[1], 'fasta')
-summary_align = AlignInfo.SummaryInfo(alignment)
-cons = summary_align.dumb_consensus(float(sys.argv[2]))
